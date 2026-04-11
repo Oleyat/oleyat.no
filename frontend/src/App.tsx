16ff63831742b6118881a7ec9b1a8f1e611c02ac
Haydar <online@oleyat.no>
2026-04-11 14:31:19 +0200

    redo struktur, mappe for sider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,55 +1,22 @@
-import './App.css'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Navbar from './components/Navbar.tsx'
-import { ColorText } from './components/decoration/ColorText.tsx';
+import Home from './pages/Home.tsx';
 
 function App() {
 
   return (
     <>
-      <div className='hidden md:block'>
-      <Navbar /></div>
+      <BrowserRouter>
 
-      <section className="center relative mt-50 w-full h-96">
-        <div className="mx-20">
-
-
-          <h1 className='md:text-8xl text-4xl  text-center'>velkommen</h1>
-          <p className='text-3xl mt-2 text-center text-slate-500/30'><ColorText text={"oleyat.no"} /></p>
+        <div className='hidden md:block'>
+          <Navbar />
         </div>
-      </section>
 
-      <section className="w-full absolute bottom-30">
-        <div id="social">
-          <h2 className='text-xl md:text-2xl text-center'>finn meg her</h2>
-          <p className='text-l md:text-2xl text-center text-slate-500/30'>eller ta kontakt gjennom skjema</p>
-          <ul className='flex flex-row my-10 gap-20 justify-center'>
-            <li>
-              <a href="https://github.com/oleyat" target="_blank">
-                <svg
-                  className="social_icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                <p className='text-slate-500/30'><ColorText text="Github" /></p>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/haydar-oleyat/" target="_blank">
-                <svg
-                  className="social_icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#linkedin-icon"></use>
-                </svg>
-                <p className='text-slate-500/30'><ColorText text="LinkedIn" /></p>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import './Navbar.css'
 import { ColorText } from './decoration/ColorText';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -7,19 +7,19 @@ function Navbar() {
       <nav className="navbar w-full h-16 bg-stone-800/30 ">
             <ul className='flex flex-row items-center justify-center gap-25 h-full text-xl'>
                 <li>
-                  <a href="/#" className='p-2'>
+                  <Link to="/#" className='p-2'>
                     <span className='text-yellow-400'>//</span><ColorText text="portfolio"/>
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/#" className='p-2'>
+                  <Link to="/#" className='p-2'>
                     <span className='text-pink-400'>//</span><ColorText text="blog" />
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/#" className='p-2'>
+                  <Link to="/#" className='p-2'>
                     <span className='text-sky-400'>//</span><ColorText text="kontakt" />
-                  </a>
+                  </Link>
                 </li>
             </ul>
       </nav>
