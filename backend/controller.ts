5a6ec8a34ebb511682bd4a7a0680bd200068a076
Haydar <online@oleyat.no>
2026-04-12 18:39:28 +0200

    article crud, database creation, database init

---
+++ backend/controller.ts
@@ -0,0 +1,38 @@
+import { db } from "./db/db.ts";
+
+
+export async function getArticle(slug: string) {
+    const article = await db
+    .selectFrom('articles')
+    .selectAll()
+    .where('slug', '=', slug)
+    .executeTakeFirst()
+    return article;
+}
+
+export async function getAllArticles() {
+    const articles = await db
+    .selectFrom('articles')
+    .selectAll()
+    .execute();
+    return articles;
+}
+
+export async function postArticle(title: string, description: string, slug: string, content: string) {
+    await db
+    .insertInto('articles')
+    .values({
+        title: title,
+        description: description,
+        slug: slug,
+        content: content
+    })
+    .execute();
+}
+
+export async function deleteArticle(id: number) {
+    await db
+    .deleteFrom('articles')
+    .where('id', '=', id)
+    .execute()
+}
